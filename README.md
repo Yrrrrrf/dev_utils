3027ea24f3ee1df0f2fcdf71ca89de5fc7503161
Fernando Bryan Reza Campos <fernandorezacampos@gmail.com>
2023-10-22 10:21:22 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,35 @@
+# Dev Utils
+
+[<img alt="github" src="https://img.shields.io/badge/github-Yrrrrrf%2Fdev__utils-58A6FF?style=for-the-badge&logo=github" height="24">](https://github.com/Yrrrrrf/dev_utils)
+[<img alt="crates.io" src="https://img.shields.io/crates/v/dev_utils.svg?style=for-the-badge&logo=rust" height="24">](https://crates.io/crates/dev_utils)
+[<img alt="docs.rs" src="https://img.shields.io/badge/docs.rs-dev__utils-66c2a5?style=for-the-badge&labelColor=555555" height="24">](https://docs.rs/dev_utils)
+
+This crate contains utilities for development. (My personal utilities, not for general use)
+
+`This crate is still in development, so it's not recommended to use it yet. There a lot of features that are not implemented yet.`
+
+## Features
+- [X] `log` - Logging utilities
+- [ ] `io` - Input/Output utilities
+- [ ] `siu` - Simple Interactive Utilities
+- [ ] `test` - Testing utilities ???? (probably not)
+- [ ] `util` - General utilities
+    - [ ] base change
+    - [ ] encyption
+    - [ ] hashing (idk becaus it's already in stdlib)
+    - [ ] encoding
+    - [ ] compression (at a later date)
+
+## Usage
+
+To use this crate, add the following to your `Cargo.toml` file:
+
+```toml
+[dependencies]
+dev_utils = "0.*"  # Add the latest version of this crate
+log = "*"  # It also depends on the log crate, so add that too
+```
+
+## [License](LICENSE)
+
+This project is licensed under the terms of the [MIT license](./LICENSE)
---
+++ src/terminal.rs
@@ -0,0 +1,161 @@
+//! Terminal Utilities
+//! 
+//! These are utilities for the terminal.
+//! It allow us to change the color of the text in the terminal.
+//! 
+//! Also have some functions to ask for input from the console.
+
+// ? Command Line Interface (CLI) related scripts  -------------------------------------------------    -------------------------------
+
+/// **Sets the foreground color** of a given string for ANSI terminal output.
+///
+/// This function takes an input string and a color specifier (abbreviated or full names)
+/// and formats the string with ANSI escape codes to set the foreground color of the text
+/// when printed to a terminal that supports ANSI color codes.
+///
+/// # Arguments
+///
+/// - `string` - The input string that you want to colorize.
+/// - `fg` - A color specifier that can be a single character or an abbreviation
+///          (e.g., "r" for red, "g" for green, "blue" for blue).
+///
+/// # Returns
+///
+/// A formatted string with ANSI escape codes for setting the foreground color.
+///
+/// # Examples
+///
+/// ```
+/// use logic_tracer::util::terminal::set_fg;
+/// 
+/// println!("{}", set_fg("Hello, Red!", "r"));
+/// println!("{}", set_fg("World in Green!", "green"));
+/// ```
+///
+/// # Color Specifiers
+///
+/// The following color specifiers are supported:
+///
+/// - `"r"` or `"red"` - Red
+/// - `"g"` or `"green"` - Green
+/// - `"b"` or `"blue"` - Blue
+/// - `"c"` or `"cyan"` - Cyan
+/// - `"m"` or `"magenta"` - Magenta
+/// - `"y"` or `"yellow"` - Yellow
+/// - `Anything else` - Default (reset color)
+/// 
+/// Any other color specifier or invalid input will reset the text color to default.
+///
+/// Note: This function is intended for use with ANSI terminal emulators that support
+/// ANSI color codes. The behavior may vary on terminals that do not support ANSI codes.
+pub fn set_fg(string: &str, fg: impl Into<String>) -> String {
+    format!("\x1b[{}m{}\x1b[0m", match fg.into().as_str() {
+        "r" | "red" => "31",       // Red
+        "g" | "green" => "32",     // Green
+        "b" | "blue" => "34",      // Blue
+        "c" | "cyan" => "36",      // Cyan
+        "m" | "magenta" => "35",   // Magenta
+        "y" | "yellow" => "33",    // Yellow
+        _ => "0",                  // Default (reset color)
+    }, string)
+}
+
+
+/// **Sets the background color** of a given string for ANSI terminal output.
+///
+/// This function takes an input string and a color specifier (abbreviated or full names)
+/// and formats the string with ANSI escape codes to set the background color of the text
+/// when printed to a terminal that supports ANSI color codes.
+///
+/// # Arguments
+///
+/// - `string` - The input string that you want to colorize.
+/// - `bg` - A color specifier that can be a single character or an abbreviation
+///          (e.g., "r" for red, "g" for green, "blue" for blue).
+///
+/// # Returns
+///
+/// A formatted string with ANSI escape codes for setting the background color.
+///
+/// # Examples
+///
+/// ```
+/// use logic_tracer::util::terminal::set_bg;
+///
+/// println!("{}", set_bg("Hello, Red Background!", "r"));
+/// println!("{}", set_bg("World in Green Background!", "green"));
+/// ```
+///
+/// # Color Specifiers
+///
+/// The following color specifiers are supported:
+///
+/// - `"r"` or `"red"` - Red
+/// - `"g"` or `"green"` - Green
+/// - `"b"` or `"blue"` - Blue
+/// - `"c"` or `"cyan"` - Cyan
+/// - `"m"` or `"magenta"` - Magenta
+/// - `"y"` or `"yellow"` - Yellow
+///
+/// Any other color specifier or invalid input will reset the background color to default.
+///
+/// Note: This function is intended for use with ANSI terminal emulators that support
+/// ANSI color codes. The behavior may vary on terminals that do not support ANSI codes.
+pub fn set_bg(string: &str, bg: impl Into<String>) -> String {
+    format!("\x1b[48;5;{}m{}\x1b[0m", match bg.into().as_str() {
+        "r" | "red" => "196",     // Red background
+        "g" | "green" => "46",   // Green background
+        "b" | "blue" => "44",    // Blue background
+        "c" | "cyan" => "51",    // Cyan background
+        "m" | "magenta" => "52", // Magenta background
+        "y" | "yellow" => "226", // Yellow background
+        _ => "49",               // Default (reset background)
+    }, string)
+}
+
+
+
+// ? Ask for input  ---------------------------------------------------------------------------------------------------------------
+
+
+use::std::io;  // io library is part of the standard library (std)
+use::std::io::Write;  // io library is part of the standard library (std) (Write trait)
+use std::str::FromStr;  // io library is part of the standard library (std) (Read trait)
+
+
+/// Ask for input from the console
+/// 
+/// ### Parameters:
+/// - `T: std::str::FromStr` - The type of the input
+/// 
+/// ### Returns:
+/// - `T` - The input
+pub (crate) fn ask<T: std::str::FromStr>() -> T where <T as FromStr>::Err: std::fmt::Debug {
+    let mut input = String::new();  // String::new() is a constructor (used when you want to modify a string)
+    print!("Enter something: ");
+    io::stdout().flush().unwrap();  // Allows the print!() to be flushed to the console otherwise it will wait for the next println!()
+    io::stdin().read_line(&mut input).unwrap();  // Read input from the console
+    println!("You entered: {}", input.trim());  // Trim the input to remove the newline character
+    return input.trim().parse::<T>().unwrap();
+}
+
+
+/// Print the type of a variable
+/// 
+/// ### Parameters:
+/// - `_: &T` - The variable to print the type of
+pub (crate) fn print_type_of<T>(_: &T) {
+    println!("{}", std::any::type_name::<T>())
+}
+
+
+/// Return the type of a variable as a string
+/// 
+/// ### Parameters:
+/// - `_: &T` - The variable to get the type of
+/// 
+/// ### Returns:
+/// - [`String`] - The type of the variable
+pub (crate) fn get_type_of<T>(_: &T) -> String {
+    return std::any::type_name::<T>().to_string();
+}
